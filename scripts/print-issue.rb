8c15ed6f1d6a437d0519e1635b07ab171831651c
Austin C Roos <austin.roos@wetransfer.com>
2022-07-20 19:05:47 +0200

    Update script to print issue details

--- scripts/print-issue.rb
+++ scripts/print-issue.rb
@@ -1,3 +1,13 @@
-puts 'Printing issue'
-puts ARGV
-puts 'Printed issue'
+require 'yaml'
+
+issue_body = ARGV[0]
+puts "Raw issue body:\n#{issue_body}"
+
+issue_content = YAML.load(issue_body)
+puts "Parsed issue body:\n#{issue_content}"
+
+email = issue_content['email']
+role = issue_content['role']
+
+puts "Email: #{email}"
+puts "Role: #{role}"
