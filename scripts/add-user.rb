0c31070d6c9dbda359a1e8e14ca3a0334c41c897
Austin C Roos <austin.roos@wetransfer.com>
2022-07-20 19:19:36 +0200

    Acutally call method to update file

--- scripts/add-user.rb
+++ scripts/add-user.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
 def parse_issue(issue_body)
-  YAML.load(issue_body)
+  YAML.safe_load(issue_body)
 rescue => e
   puts "Error parsing issue body: #{e.message}"
   raise
@@ -12,6 +12,11 @@ def update_test_json(issue_content)
   test_json_hash = JSON.parse(test_json)
   test_json_hash['users'].push(issue_content)
   File.write('test.json', JSON.pretty_generate(test_json_hash))
+rescue => e
+  puts "Error updating test.json: #{e.message}"
+  raise
 end
 
 issue_content = parse_issue(ARGV[0])
+update_test_json(issue_content)
+puts 'done.'
